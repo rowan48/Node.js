d75469abab71b34da6744559168708fbec9da74c
git-trial <rowan.rowanahmedali11@gmail.com>
2022-04-12 22:17:46 +0200

    lab3

--- lab3/app.js
+++ lab3/app.js
@@ -1,86 +1,84 @@
+/****************************************************************************************************************************************************************************************/
+//===================================================routing using http module==========================================================================================================
 
-//routing
-var http = require('http')
-var fs = require('fs')
- const pathsync = require("path");
+var http = require('http')//requiring the http module
+var fs = require('fs')//require the fs module to open files
+const pathsync = require("path");//to detect the paths if needed
 http.createServer(function(req, res){
-    const buffers = [];
-
+ //========================================================GET Request Handling========================================================================================================
     if(req.url==='/' && req.method==='GET')
     {
         res.writeHead(200, {'content-type': "text/html"})
         var html = fs.readFileSync('homepage.html')
-//   // recomended
-
-        obj = {
-            name: "rowan",
-            email: "rowanahmedali",
-            password:"rowan"
-        }
-        res.write(html)
+         res.write(html)
     }
-	else if(req.url==='/signup' && req.method==='POST' ){
-	// obj = {
-	// 	name: "rowan",
-	// 	email: "rowanahmedali",
-    //     password:"rowan"
-	// }
-        // console.log("json data");
-        obj=req.body;
-        console.log("obiid======================================================");
-        console.log(obj);
-         // console.log("res");
-        // console.log(res);
-        // console.log(req);
-        let data =JSON.stringify(obj);
-        //reading the data in the text file to check iif the data entered already exist or not
-        let rawdata = fs.readFileSync(pathsync.resolve(__dirname, 'user.txt'));
-        let iti = JSON.parse(rawdata);
-        if(JSON.stringify(iti) === JSON.stringify(obj)){
-            res.writeHead(400, {'content-type': "text/plain"});
-            res.write("user already exist");
-
-        }else{
-            //==============================================writing the data into the file===================================================================
-            res.writeHead(200, {'content-type': "application/json"})
-            fs.writeFileSync(pathsync.resolve(__dirname, './user.txt'), data);
-            console.log("finish writiing");
-            // ==============================================writing the data into the file====================================================================
-
-            res.write(JSON.stringify(obj))
-
-        }
-
-
+    //========================================================POST Request Handling For signup page========================================================================================================
+/* first we will check if the data entered already exixts or not
+* if exist will display a message to the   user that he/she already have an account
+* if not will add the user's data to the user.txt file
+* */
+    else if(req.url==='/signup' && req.method==='POST' ){
+        req.on('data', obj => {
+            body += obj;//holding the data of the user on another object to use it
+            //=>console.log(`Data chunk available: ${obj}`)//t is used to check the data that the user sent
+        // //reading the data in the text file to check iif the data entered already exist or not
+       let rawdata = fs.readFileSync(pathsync.resolve(__dirname, 'user.txt'));
+        console.log(rawdata);
+           let iti = JSON.parse(rawdata);
+           console.log(iti);
+            console.log(`chunk : ${obj}`);
+               if(iti.email===obj.email || iti.name===obj.name || iti.password===obj.password){//check if the user;s data already exist
+               res.writeHead(400, {'content-type': "text/plain"});
+               res.write("user already exist");
+           } else {//if not
+               //==============================================writing the data into the file===================================================================
+               res.writeHead(200, {'content-type': "application/json"})
+               fs.writeFileSync(pathsync.resolve(__dirname, './user.txt'), obj);
+               console.log("finish writiing");
+               console.log((obj));
+               res.end();
+           }
+            })
 }
-else if (req.url==='/login' && req.method==='POST'){
-        obj = {
-            name: "rowan",
-            email: "rowanahmedali",
-            password:"rowan"
-        }
-        console.log(req);
-
-        let rawdata = fs.readFileSync(pathsync.resolve(__dirname, 'user.txt'));
-        let iti = JSON.parse(rawdata);
-        if (obj.email != iti.email){
-            res.writeHead(400, {'content-type': "application/json"})
-            console.log("the email is wrong");
-        }else if (obj.password !=iti.password){
-            res.writeHead(400, {'content-type': "application/json"})
-
-            console.log("the password is wrong")
-        }else{
-            res.writeHead(200, {'content-type': "application/json"})
-            res.write(JSON.stringify(iti))
-
-        }
+    //========================================================POST Request Handling For Login page========================================================================================================
+    /*read the data from the file to check if it is found or not
+    * if found redirect him to the login with welcome message
+    * if not define which data is wrong{name,email,password}
+    * */
+    else if (req.url==='/login' && req.method==='POST'){
+        var body=" ";
+        req.on('data', obj => {
+            body += obj;
+            let bodyj =JSON.parse(body);//parsing the user's object to use it on the check's
+            //=>console.log(`Data chunk available: ${obj}`)//checking on the data only on the console if used
+            let rawdata = fs.readFileSync(pathsync.resolve(__dirname, 'user.txt'));
+            let iti = JSON.parse(rawdata);
+            if (bodyj.email != iti.email) {
+                res.writeHead(400, {'content-type': "application/json"})
+                message="the email is wrong";
+            } else if (bodyj.password != iti.password) {
+                res.writeHead(400, {'content-type': "application/json"})
+                message="the password is wrong";
+            } else {
+                res.writeHead(200, {'content-type': "application/json"})
+                message = bodyj.name;
+            }
+            res.writeHead(200, {'content-type': "text/html"})
+            var html = fs.readFileSync('./login.html', 'utf8');
+            html = html.replace('{message}', message);
+            console.log("welcome "+message);//check on the welcome message through the console until adding it to the html file
+            res.end();
+        })
 }
+    //========================================================Handling UnKnown Requests========================================================================================================
+
     else
     {
         res.writeHead(404)
         res.write('page is not found');
+        message="page not found";
     }
     res.end()
 //
 }).listen(3000)
+/************************************************************************************************************************************************************************************************/
